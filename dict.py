665510ef2cf8939484a1eb89d83fea1b7fddfdb6
Charlie Brooking <cbrookin@thoughtworks.com>
2017-09-17 20:49:40 +1000

    complete notes on basic dict

--- dict.py
+++ dict.py
@@ -1,11 +1,25 @@
 assert dict() == {}, 'alternative ways to construct dictionaries'
 
-grades = {'Joel': 80, 'Tim': 95}
+grades = {'Joel': 80, 'Elle': 95}
+grades['Tim'] = 75
 assert grades['Joel'] == 80
-assert grades['Tim'] == 95
+assert grades['Elle'] == 95
+assert grades['Tim'] == 75
 
 try:
   grades['Foo']
   assert False, 'indexing on unknown key raises error'
 except KeyError:
   pass
+
+assert grades.get('Joel') == 80, 'get method is an alternative to indexing'
+assert grades.get('Kate') == None, 'get returns None as deault instead of raising KeyError'
+assert grades.get('Kate', 0) == 0, 'get can return a given default value instead'
+
+assert len(grades) == 3
+assert sorted(grades.keys()) == sorted(['Joel', 'Elle', 'Tim'])
+assert sorted(grades.values()) == sorted([80, 95, 75])
+
+assert 'Joel' in grades
+assert 'Kate' not in grades
+assert ('Elle' in grades) == ('Elle' in grades.keys()), 'in expr checks key membership'
