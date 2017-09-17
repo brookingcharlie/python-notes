a6894bb8b477f1be3e933edf5dba1a6d447a2dba
Charlie Brooking <cbrookin@thoughtworks.com>
2017-09-18 00:07:12 +1000

    add unpacking/args/kwargs notes

--- functions.py
+++ functions.py
@@ -24,3 +24,22 @@ assert pow(n=2, m=3) == 8, 'passing both arguments by name'
 assert pow(m=3, n=2) == 8, 'passing both arguments by name in different order'
 assert pow(n=1) == 1, 'passing only first argument by name, using defult for second'
 assert pow(m=2) == 4, 'passing only second argument by name, using defult for first'
+
+# argument unpacking
+
+def add(a, b): return a + b
+assert add(1, 2) == 3
+assert add(*[1, 2]) == 3
+
+# args and kwargs
+
+def magic(*args, **kwargs):
+  return \
+    ['%d: %s' % (i, a) for i, a in enumerate(args)] + \
+    ['%s: %s' % (i, k) for i, k in sorted(kwargs.items())]
+
+assert magic(1, 2, x=5, y=7) == ['0: 1', '1: 2', 'x: 5', 'y: 7']
+
+args = [12, 34]
+kwargs = {'a': 1, 'b': 2}
+assert magic(*args, **kwargs) == ['0: 12', '1: 34', 'a: 1', 'b: 2']
